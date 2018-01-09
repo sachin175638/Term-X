3d6cc665c2a277ca9497184cf16c16488fef2a75
sachin175638 <35227965+sachin175638@users.noreply.github.com>
2018-01-09 10:07:06 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $HOME
+apt-get install python2 -y
+apt-get install figlet -y
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd Term-X
+chmod +x turrmux.sh
+echo "run sh turmux.sh "
+
